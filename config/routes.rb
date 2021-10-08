4203e35f58abc5122f1b012e0aa7c4abfec94954
yu-M215 <yuu0609km.215@gmail.com>
2021-10-08 06:51:06 +0000

    [Add] マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 end
