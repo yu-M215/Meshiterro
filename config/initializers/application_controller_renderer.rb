d9d62444b0a11b51d1c29ed7ca78c6622f7027c0
yu-M215 <yuu0609km.215@gmail.com>
2021-10-08 06:18:50 +0000

    [Add] 投稿詳細ページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '54363b0adec0d5621d5c08ab031ed9ec309a18b05fb51ffbf71cee7f183da4e63f6af2d27df42780f750ed99e1facdf99b8f6d5e3a82239100fb03ad8db3ccfe'
